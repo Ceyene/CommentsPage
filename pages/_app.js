ac89e95c14f5901b4bbd776c92ec8602d28232f4
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-06 14:46:54 -0300

    Adding head tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { Fragment } from 'react';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Fragment>
+			<Head>
+				<meta charSet="utf-8" />
+				<title>Comments Page - by Cyn Romero</title>
+				<meta
+					name="description"
+					content="A small application for practice server-side code on Next.js"
+				/>
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</Head>
+			<Component {...pageProps} />
+		</Fragment>
+	);
 }
 
-export default MyApp
+export default MyApp;
